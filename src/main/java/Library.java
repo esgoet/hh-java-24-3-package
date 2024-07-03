c0f84837d43cd45335bf38c6363b95357e3e9be1
esgoet <toddesgo@gmail.com>
2024-07-03 17:02:21 +0200

    Add add and delete func

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,13 +1,52 @@
+//import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Library {
     private Book[] books;
+//    private ArrayList<Book> books = new ArrayList<Book>();
 
     public Library() {
         books = new Book[10];
     }
     public Library(Book[] books) {
         this.books = books;
+    }
+
+//    public Library(ArrayList<Book> books) {
+//        this.books = books;
+//    }
+
+    public void addBook(Book book) {
+        Book[] newBooks = new Book[books.length + 1];
+        for (int i = 0; i < newBooks.length -1; i++) {
+            newBooks[i] = books[i];
+        }
+        newBooks[newBooks.length - 1] = book;
+        books = newBooks;
+
+        //with ArrayList
+//        books.add(book);
+
+    }
+
+    public void deleteBook(Book book) {
+        Book[] newBooks = new Book[books.length - 1];
+        int currentIndex = 0;
+        for (int i = 0; i < newBooks.length; i++) {
+            if (!books[i].equals(book)) {
+                newBooks[i] = books[i];
+            } else {
+                currentIndex = i;
+                break;
+            }
+        }
+        for (int i = currentIndex; i < newBooks.length; i++) {
+            newBooks[i] = books[i + 1];
+        }
+        books = newBooks;
+
+        // with ArrayList
+//        books.remove(book);
     }
 
     public String toString() {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,14 @@ public class Main {
     public static void main(String[] args) {
         Book book1 = new Book("Pride and Prejudice", "Jane Austen", 1243456645);
         Book book2 = new Book("Emma", "Jane Austen", 1743894732);
+        Book book3 = new Book("Mansfield Park", "Jane Austen", 1243454645);
         Library library = new Library(new Book[] {book1, book2});
 
         System.out.println(library);
-
+        library.addBook(book3);
+        System.out.println(library);
+        library.deleteBook(book2);
+        System.out.println(library);
 
 
     }
